8a4719f55b8dde3c535434ef5305c0d7cb881645
C-D-Lewis <Bonsitm@gmail.com>
2024-11-19 22:17:32 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default {
-  base: 'dist',
+  base: 'dist',  // For terraform-modules/s3-cloudfront-website serving of dist/index.html
   plugins: [
     viteStaticCopy({
       targets: [
